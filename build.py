0be70b6633b3c8bf4d84fc3192abcc9eadfa705b
LeeviT <leevi.tuikka@helsinki.fi>
2018-04-03 21:35:10 +0300

    harjoitustyö fileReader

---
+++ build.py
@@ -0,0 +1,10 @@
+from pybuilder.core import use_plugin
+from pybuilder.core import init
+
+use_plugin("python.core")
+
+#@init
+#def initialize (project):
+#    project.set_property("/", 'scripts')
+
+default_task = "publish"
