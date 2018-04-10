bf0f9eca78329574bac153b40e5a18d573c99cc0
LeeviT <leevi.tuikka@helsinki.fi>
2018-04-10 16:17:24 +0300

    quadtree

--- src/main/python/structures/quadtree.py
+++ src/main/python/structures/quadtree.py
@@ -2,18 +2,41 @@ import math
 from enum import Enum
 from main.python.structures.body import Body
 
-class NodeStorage:
+
+class NodeStorage():
+
+    def __init__(self):
+        self.nodeList = []
+        self.storageSize = 0
+
+    def addNodeToStorage(self, nodeID, node):
+        self.nodeList.append([nodeID, node])
+        self.storageSize += 1
+
+    def printNodeStorage(self):
+        for i in range(self.storageSize):
+            print(self.nodeList[i][0], self.nodeList[i][1].getInfo())
+
+    def getSize(self):
+        return self.storageSize
+
+    def getNodeUsingID(self, nodeID):
+        for i in range(self.storageSize):
+            if (self.nodeList[i][0] == nodeID)
+                return self.nodeList[i][1]
+
 
 class NodeDirection(Enum):
     SW = 'SW'
     NW = 'NW'
     NE = 'NE'
     SE = 'SE'
+    ROOT = 'ROOT'
+
 
 class Node:
 
     def __init__(self, x0, xh, y0, yh, direction):
-        self.id = nodeId
         self.x0 = x0
         self.x1 = xh
         self.y0 = y0
@@ -28,57 +51,53 @@ class Node:
         else:
             return False
 
-    def addBodyToNode(self, body):
-        
+    # def addBodyToNode(self, body, nodeStorage):
 
-    def divideNode(self):
-        SWNode = Node(self.x0, self.x1/2.0, self.y0, self.y1/2.0, NodeDirection.SW)
-        NWNode = Node(self.x0, self.x1/2.0, self.y1/2.0, self.y1, NodeDirection.NW)
-        NENode = Node(self.x1/2.0, self.x1, self.y1/2.0, self.y1, NodeDirection.NE)
-        SENode = Node(self.x1/2.0, self.x1, self.y1, self.y1/2.0, NodeDirection.SE)
+    def divideNode(self, nodeStorage):
+        SWNode = Node(self.x0, self.x1 / 2.0, self.y0, self.y1 / 2.0, NodeDirection.SW.name)
+        NWNode = Node(self.x0, self.x1 / 2.0, self.y1 / 2.0, self.y1, NodeDirection.NW.name)
+        NENode = Node(self.x1 / 2.0, self.x1, self.y1 / 2.0, self.y1, NodeDirection.NE.name)
+        SENode = Node(self.x1 / 2.0, self.x1, self.y1, self.y1 / 2.0, NodeDirection.SE.name)
+        nodeStorage.addNodeToStorage(SWNode.generateNodeID(0), SWNode)
+        nodeStorage.addNodeToStorage(NWNode.generateNodeID(0), NWNode)
+        nodeStorage.addNodeToStorage(NENode.generateNodeID(0), NENode)
+        nodeStorage.addNodeToStorage(SENode.generateNodeID(0), SENode)
 
-
-
-    def addBodyToQuadtree(self, body):
-        for i in range(len(nodes)):
-            if self.isBodyInNode(body) == True:
-                if self.isEmpty == True:
-                    if self.hasChildren == False:
-                        self.addBodyToNode(body)
-                    elif self.hasChildren == True:
-                        self.divideNode()
-                elif self.isEmpty == False:
-
+    def addBodyToQuadtree(self, body, nodeStorage):
+        if self.isBodyInNode(body) == True:
+            if self.isEmpty == True:
+                if self.hasChildren == False:
+                    self.divideNode(nodeStorage)
+                    #self.addBodyToNode(body, nodeStorage)
+                elif self.hasChildren == True:
+                    self.divideNode(nodeStorage)
+            # elif self.isEmpty == False:
 
     def generateNodeID(self, parentNodeID):
-        if self.direction == 'Sw':
-            return parentNodeID + '1'
+        if self.direction == 'ROOT':
+            return parentNodeID
+        elif self.direction == 'SW':
+            return int(str(parentNodeID) + '1')
         elif self.direction == 'NW':
-            return parentNodeID + '2'
+            return int(str(parentNodeID) + '2')
         elif self.direction == 'NE':
-            return parentNodeID + '3'
+            return int(str(parentNodeID) + '3')
         elif self.direction == 'SE':
-            return parentNodeID + '4'
-
+            return int(str(parentNodeID) + '4')
 
     def getInfo(self):
-        return self.id, self.x0, self.x1, self.y0, self.y1
+        return self.x0, self.x1, self.y0, self.y1
 
     def getLevel(self):
-        return int(math.log(1/(self.x1 - self.x0), 2))
+        return int(math.log(1 / (self.x1 - self.x0), 2))
 
-global nodeId
-global nodes
-nodeId = 1
-nodes = []
-nodes.append(Node(0.0, 0.5, 0.0, 0.5))
-nodeId += 1
-nodes.append(Node(0.0, 0.5, 0.5, 0.5))
-nodeId += 1
-nodes.append(Node(0.5, 0.5, 0.5, 0.5))
-nodeId += 1
-nodes.append(Node(0.5, 0.5, 0.0, 0.5))
-testBody = Body(1, 1.2, 0.51, 0.51, 0.1, 0.1)
-addBodyToQuadtree(testBody)
-for i in range(len(nodes)):
-    print(nodes[i].isEmpty)
+
+nodeStorage = NodeStorage()
+rootNode = Node(0.0, 1.0, 0.0, 1.0, 'ROOT')
+testBody = Body(1, 1.2, 0.1, 0.1, 0.01, 0.01)
+#rootNode.divideNode(nodeStorage)
+#testBody2 = Body(1, 1.2, 0.15, 0.15, 0.01, 0.01)
+#rootNode.divideNode(nodeStorage)
+rootNode.addBodyToQuadtree(testBody, nodeStorage)
+nodeStorage.printNodeStorage()
+
